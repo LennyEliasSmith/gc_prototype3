2b5f772f72f987c0a3468d390a1a20d549b67d8b
Lenny Smith <lenny.elias.s@gmail.com>
2020-06-16 14:44:11 +0300

    UIChanges

--- Assets/Scripts/UIScripts/compendiumCell.cs
+++ Assets/Scripts/UIScripts/compendiumCell.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class compendiumCell
-{
+{   
+    //Fish Data
     public SpriteRenderer _cellSprite;
-
+    public string _cellName;
     public string _cellData;
+
+   
 
 }
---
+++ Assets/Scripts/UIScripts/compendiumMaster.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compendiumMaster : MonoBehaviour
+{
+     public GameObject[] _fishCells;
+
+    public string cell1Data;
+    public SpriteRenderer cell1Sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        compendiumCell cell1 = new compendiumCell();
+        cell1._cellSprite = cell1Sprite;
+        cell1._cellName = "Baglas";
+        cell1._cellData = cell1Data;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
